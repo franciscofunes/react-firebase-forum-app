0231476da921178de319cfed843b846e2c8decb5
franciscofunes <48732867+franciscofunes@users.noreply.github.com>
2022-01-30 00:52:20 -0300

    feat: Invitation link in a button

--- src/pages/Casamiento.js
+++ src/pages/Casamiento.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from "react";
 
 function Casamiento({ isAuth }) {
 
+  const openVideo = () => {
+    window.location.href = 'https://www.canva.com/design/DAE17nAB-xw/nsmalc5iv5oTFP9mkbk30g/view';
+};
   return (
     <div className="Casamiento">
-      <h1>Nos casamos</h1>
+      <button onClick={openVideo}>Invitación</button>
+<iframe src="https://www.canva.com/design/DAE17nAB-xw/nsmalc5iv5oTFP9mkbk30g/view"></iframe>
+<iframe src="https://platform.twitter.com/widgets/tweet_button.html"></iframe>
     </div>
   );
 }
