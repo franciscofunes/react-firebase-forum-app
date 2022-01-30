50ae45402e19c2cf0c7b974871fde8c873aa1903
franciscofunes <48732867+franciscofunes@users.noreply.github.com>
2022-01-30 00:25:44 -0300

    Created use scroll.js

---
+++ src/components/Navbar/UseScroll.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+
+function MyApp () {
+
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset);
+        // clean up code
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
+    console.log(offset); 
+};
